0d1057878270d0c7bbe901b5274a717b329227af
TliliOS <81974387+TLILIOS@users.noreply.github.com>
2022-04-22 04:26:30 +0200

    Mis ajour 2

---
+++ Model/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  InstagridP4
+//
+//  Created by HAMDI TLILI on 10/03/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Model/View/Base.lproj/GridViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagrid
+//
+//  Created by HAMDI TLILI on 20/02/2022.
+//
+
+import UIKit
+
+class GridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
